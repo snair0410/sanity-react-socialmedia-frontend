0fcfd40788561459581049dd29f462ebc9da9fa4
Sandeep Nair <sandeepnair@Sandeeps-MacBook-Air.local>
2022-06-02 09:47:25 +0530

    second commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import GoogleLogin from 'react-google-login'
 import { useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
-import shareVideo from '../assets/share.mp4'
+// import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
 import { client } from '../client'
 
@@ -30,7 +30,7 @@ const Login = () => {
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
-        <video
+        {/* <video
           src={shareVideo}
           type="video/mp4"
           loop
@@ -38,7 +38,12 @@ const Login = () => {
           muted
           autoPlay
           className='w-full h-full object-cover'
-        />
+        /> */}
+        <img
+              className=" w-full h-full 2xl:h-510 shadow-lg object-cover"
+              src="https://source.unsplash.com/1600x900/?nature,photography,technology"
+              alt="user-pic"
+            />
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
           <div className='p-5'>
             <img src={logo} width='130px' alt='logo' />
